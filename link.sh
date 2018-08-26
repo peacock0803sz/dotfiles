663bd02a2d9ea24eada223cc82d2f29a4555b5b3
peacock0803sz <peacock0803sz@gmail.com>
2018-08-26 23:37:17 +0900

    fix link

--- link.sh
+++ link.sh
@@ -2,6 +2,7 @@
 
 cd ~/
 
-ln -s dotfiles/.hyper.js .hyper.js
-ln -s dotfiles/.zshrc .zshrc
-ln -s dotfiles/.vimrc .vimrc
+ln -s ~/dotfiles/.hyper.js .hyper.js
+ln -s ~/dotfiles/.zshrc .zshrc
+ln -s ~/dotfiles/.vimrc .vimrc
+ln -s ~/dotfiles/.config/rofi rofi

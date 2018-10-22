87ca4a1b04065eefd33704b94298a83783ba9bc1
peacock0803sz <claude0803sz@gmail.com>
2018-10-22 14:14:49 +0900

    fix link

--- link.sh
+++ link.sh
@@ -5,4 +5,5 @@ cd ~/
 ln -s ~/dotfiles/.hyper.js .hyper.js
 ln -s ~/dotfiles/.zshrc .zshrc
 ln -s ~/dotfiles/.vimrc .vimrc
-ln -s ~/dotfiles/.config/rofi rofi
+ln -s ~/dotfiles/.config/rofi .config/rofi
+ln -s ~/dotfiles/tmux.conf .tmux.conf

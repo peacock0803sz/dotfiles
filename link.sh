92aa6d153140be56cfc57a102ccc45a1c008173e
Peacock0803sz <claude0803sz@gmail.com>
2018-12-18 05:03:54 +0900

    fix

--- link.sh
+++ link.sh
@@ -7,5 +7,5 @@ ln -s ~/dotfiles/.zshrc .zshrc
 ln -s ~/dotfiles/.vimrc .vimrc
 ln -s ~/dotfiles/.config/rofi .config/rofi
 ln -s ~/dotfiles/tmux.conf .tmux.conf
-ln -s ~/dotfiles/.config/Code/User/settings.json ~/.config/Code/User/settings.json 
+ln -s ~/dotfiles/.config/Code-OSS/User/settings.json ~/.config/Code\ -\ OSS/User/settings.json 
 ln -s ~/dotfiles/.xprofile .xprofiles

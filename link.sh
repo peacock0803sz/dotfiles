06a4352a7b0edf7a58014a69f0f60fa4197673fe
Peacock0803sz <claude0803sz@gmail.com>
2018-12-19 22:18:06 +0900

    link to .init.vim

--- link.sh
+++ link.sh
@@ -2,9 +2,9 @@
 
 cd ~/
 
-ln -s ~/dotfiles/.hyper.js .hyper.js
 ln -s ~/dotfiles/.zshrc .zshrc
 ln -s ~/dotfiles/.vimrc .vimrc
+ln -s ~/dotfiles/.config/nvim/init.vim .config/nvim/init.vim
 ln -s ~/dotfiles/.config/rofi .config/rofi
 ln -s ~/dotfiles/tmux.conf .tmux.conf
 ln -s ~/dotfiles/.config/Code/User/settings.json ~/.config/Code/User/settings.json 

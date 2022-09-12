be45f8bb077af717572f7ea7b1b42f313a271324
Yoichi Takai <contact@peacock0803sz.com>
2022-09-12 16:30:43 +0900

    Update nvim keymap

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -2,30 +2,29 @@ local M = {}
 
 local opts = { noremap=true, silent=true }
 -- c-mode
-vim.api.nvim_set_keymap("c", "<C-a>", "<Home>", opts)
-vim.api.nvim_set_keymap("c", "<C-b>", "<Left>", opts)
-vim.api.nvim_set_keymap("c", "<C-e>", "<End>", opts)
-vim.api.nvim_set_keymap("c", "<C-f>", "<Right>", opts)
-vim.api.nvim_set_keymap("c", "<C-n>", "<Down>", opts)
-vim.api.nvim_set_keymap("c", "<C-p>", "<Up>", opts)
+vim.keymap.set("c", "<C-a>", "<Home>", opts)
+vim.keymap.set("c", "<C-b>", "<Left>", opts)
+vim.keymap.set("c", "<C-e>", "<End>", opts)
+vim.keymap.set("c", "<C-f>", "<Right>", opts)
+vim.keymap.set("c", "<C-n>", "<Down>", opts)
+vim.keymap.set("c", "<C-p>", "<Up>", opts)
 
 -- scroll
-vim.api.nvim_set_keymap("", "<ScrollWheelUp>", "<C-Y>", opts)
-vim.api.nvim_set_keymap("", "<ScrollWheelDown>", "<C-E>", opts)
+vim.keymap.set("", "<ScrollWheelUp>", "<C-Y>", opts)
+vim.keymap.set("", "<ScrollWheelDown>", "<C-E>", opts)
 
 -- tab
-vim.api.nvim_set_keymap("n", "[TABCMD]", "<nop>", opts)
-vim.api.nvim_set_keymap("n", "<leader>t", "[TABCMD]", opts)
-vim.api.nvim_set_keymap("n", "[TABCMD]f", ":<C-u>tabfirst<CR>", opts)
-vim.api.nvim_set_keymap("n", "[TABCMD]l", ":<C-u>tablast<CR>", opts)
-vim.api.nvim_set_keymap("n", "[TABCMD]n", ":<C-u>tabnext<CR>", opts)
-vim.api.nvim_set_keymap("n", "[TABCMD]N", ":<C-u>tabNext<CR>", opts)
-vim.api.nvim_set_keymap("n", "[TABCMD]p", ":<C-u>tabprevious<CR>", opts)
-vim.api.nvim_set_keymap("n", "[TABCMD]e", ":<C-u>tabedit<CR>", opts)
-vim.api.nvim_set_keymap("n", "[TABCMD]w", ":<C-u>tabclose<CR>", opts)
-vim.api.nvim_set_keymap("n", "[TABCMD]o", ":<C-u>tabonly<CR>", opts)
-vim.api.nvim_set_keymap("n", "[TABCMD]c", ":<C-u>tabs<CR>", opts)
-vim.api.nvim_set_keymap("n", "[TABCMD]s", ":<C-u>tabnew<CR>", opts)
+vim.keymap.set("n", "<leader>t", "<Plug>(tab)", opts)
+vim.keymap.set("n", "<Plug>(tab)f", ":<C-u>tabfirst<CR>", opts)
+vim.keymap.set("n", "<Plug>(tab)l", ":<C-u>tablast<CR>", opts)
+vim.keymap.set("n", "<Plug>(tab)n", ":<C-u>tabnext<CR>", opts)
+vim.keymap.set("n", "<Plug>(tab)N", ":<C-u>tabNext<CR>", opts)
+vim.keymap.set("n", "<Plug>(tab)p", ":<C-u>tabprevious<CR>", opts)
+vim.keymap.set("n", "<Plug>(tab)e", ":<C-u>tabedit<CR>", opts)
+vim.keymap.set("n", "<Plug>(tab)w", ":<C-u>tabclose<CR>", opts)
+vim.keymap.set("n", "<Plug>(tab)o", ":<C-u>tabonly<CR>", opts)
+vim.keymap.set("n", "<Plug>(tab)c", ":<C-u>tabs<CR>", opts)
+vim.keymap.set("n", "<Plug>(tab)s", ":<C-u>tabnew<CR>", opts)
 
 M.opts = opts
 return M

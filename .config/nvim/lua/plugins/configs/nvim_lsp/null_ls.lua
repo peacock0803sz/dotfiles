5f4dc467cb812235dc25362cd32c1e70ce57c499
Yoichi TAKAI <contact@peacock0803sz.com>
2022-11-28 03:47:39 +0900

    Update null_ls for flake8 args

--- .config/nvim/lua/plugins/configs/nvim_lsp/null_ls.lua
+++ .config/nvim/lua/plugins/configs/nvim_lsp/null_ls.lua
@@ -2,7 +2,9 @@ local null_ls = require("null-ls")
 
 -- sources setup
 local sources = {
-  null_ls.builtins.diagnostics.flake8,
+  null_ls.builtins.diagnostics.flake8.with({
+    extra_args = {"--max-line-length", "88"}
+  }),
   null_ls.builtins.formatting.black,
 }
 null_ls.setup({

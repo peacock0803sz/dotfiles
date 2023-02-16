a2e16a1948ff94efae274ec056d99b672e3a7f13
Yoichi Takai <contact@peacock0803sz.com>
2023-02-17 00:36:22 +0900

    migrate to lazy

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,6 +3,19 @@ vim.env.lsp_provider = lsp_provider
 
 require("options")
 
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
 require("plugins.list")
 
 -- For emergency, uncomment belows
--- .config/nvim/lua/plugins/list.lua
+++ .config/nvim/lua/plugins/list.lua
@@ -1,112 +1,80 @@
-local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({
-      "git",
-      "clone",
-      "--depth",
-      "1",
-      "https://github.com/wbthomason/packer.nvim",
-      install_path,
-    })
-    vim.cmd([[packadd packer.nvim]])
-    return true
-  end
-  return false
-end
+require("lazy").setup({
+  { "folke/neoconf.nvim", cmd = "Neoconf" },
 
-local packer_bootstrap = ensure_packer()
+  { "vim-jp/vimdoc-ja" },
 
-return require("packer").startup(function(use)
-  use("wbthomason/packer.nvim")
+  { "nvim-lua/plenary.nvim" },
+  { "lambdalisue/nerdfont.vim" },
 
-  use({ "vim-jp/vimdoc-ja" })
+  { "lambdalisue/guise.vim" },
 
-  use({ "nvim-lua/plenary.nvim" })
-  use({ "lambdalisue/nerdfont.vim" })
+  { "rmehri01/onenord.nvim" },
+  { "catppuccin/nvim", name = "catppuccin" },
 
-  use({ "lambdalisue/guise.vim" })
+  { "nvim-lualine/lualine.nvim" },
+  { "akinsho/bufferline.nvim" },
 
-  -- use({ "rmehri01/onenord.nvim" })
-  use({ "catppuccin/nvim", as = "catppuccin" })
+    { "hrsh7th/nvim-cmp" },
+    { "hrsh7th/cmp-nvim-lsp" },
+    { "hrsh7th/cmp-buffer", dependencies = { "nvim-cmp" } },
+    { "hrsh7th/cmp-path", dependencies = { "nvim-cmp" } },
+    { "hrsh7th/cmp-cmdline", dependencies = { "nvim-cmp" } },
+    { "hrsh7th/cmp-omni", dependencies = { "nvim-cmp" } },
+    { "f3fora/cmp-spell", dependencies = { "nvim-cmp" } },
 
-  use({ "nvim-lualine/lualine.nvim" })
-  use({ "akinsho/bufferline.nvim" })
+    { "onsails/lspkind.nvim" },
 
-  if vim.env.lsp_provider == "nvim_lsp" then
-    use({ "hrsh7th/nvim-cmp" })
-    use({ "hrsh7th/cmp-nvim-lsp" })
-    use({ "hrsh7th/cmp-buffer", after = { "nvim-cmp" } })
-    use({ "hrsh7th/cmp-path", after = { "nvim-cmp" } })
-    use({ "hrsh7th/cmp-cmdline", after = { "nvim-cmp" } })
-    use({ "hrsh7th/cmp-omni", after = { "nvim-cmp" } })
-    use({ "f3fora/cmp-spell", after = { "nvim-cmp" } })
+    { "L3MON4D3/LuaSnip" },
+    { "saadparwaiz1/cmp_luasnip" },
 
-    use({ "onsails/lspkind.nvim" })
+    { "neovim/nvim-lspconfig" },
+    { "williamboman/mason.nvim" },
+    { "williamboman/mason-lspconfig.nvim" },
 
-    use({ "L3MON4D3/LuaSnip" })
-    use({ "saadparwaiz1/cmp_luasnip" })
+    { "kkharji/lspsaga.nvim" },
+    { "SmiteshP/nvim-navic" },
 
-    use({ "neovim/nvim-lspconfig" })
-    use({ "williamboman/mason.nvim" })
-    use({ "williamboman/mason-lspconfig.nvim" })
+    { "rcarriga/nvim-dap-ui", dependencies = { "mfussenegger/nvim-dap" } },
+    { "leoluz/nvim-dap-go" },
 
-    use({ "kkharji/lspsaga.nvim" })
-    use({ "SmiteshP/nvim-navic" })
-
-    use({ "rcarriga/nvim-dap-ui", requires = { "mfussenegger/nvim-dap" } })
-    use({ "leoluz/nvim-dap-go" })
-
-    use({ "jose-elias-alvarez/null-ls.nvim" })
-    use({ "folke/lsp-colors.nvim" })
-    use({
+    { "jose-elias-alvarez/null-ls.nvim" },
+    { "folke/lsp-colors.nvim" },
+    {
       "folke/trouble.nvim",
-      after = { "mason.nvim", "nvim-lspconfig" },
-      requires = { "kyazdani42/nvim-web-devicons" },
-      config = function()
-        require("trouble").setup({})
-      end,
-    })
-    use({ "folke/neodev.nvim" })
-    -- coc.nvim
-  elseif vim.env.lsp_provider == "coc" then
-    use({ "neoclide/coc.nvim", branch = "release" })
-  end
+      dependencies = { "mason.nvim", "nvim-lspconfig", "nvim-tree/nvim-web-devicons" },
+    },
+    { "folke/neodev.nvim" },
 
-  use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
-  use({ "nvim-telescope/telescope.nvim", requires = { "kyazdani42/nvim-web-devicons" } })
-  use({ "nvim-telescope/telescope-file-browser.nvim" })
+  { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+  { "nvim-telescope/telescope.nvim", dependencies = { "nvim-tree/nvim-web-devicons" } },
+  { "nvim-telescope/telescope-file-browser.nvim" },
 
-  use({ "lambdalisue/mr.vim" })
+  { "lambdalisue/mr.vim" },
 
-  use({ "kien/rainbow_parentheses.vim" })
-  use({ "machakann/vim-sandwich" })
-  use({ "cohama/lexima.vim" })
-  use({ "monaqa/dial.nvim" })
-  use({ "lambdalisue/suda.vim" })
-  use({ "mattn/emmet-vim" })
+  { "kien/rainbow_parentheses.vim" },
+  { "machakann/vim-sandwich" },
+  { "cohama/lexima.vim" },
+  { "monaqa/dial.nvim" },
+  { "lambdalisue/suda.vim" },
+  { "mattn/emmet-vim" },
 
-  use({ "rhysd/committia.vim" })
-  use({ "lambdalisue/gina.vim" })
-  use({ "lambdalisue/gin.vim", requires = { "vim-denops/denops.vim" } })
-  use({ "lewis6991/gitsigns.nvim" })
+  { "rhysd/committia.vim" },
+  { "lambdalisue/gina.vim" },
+  { "lambdalisue/gin.vim", dependencies = { "vim-denops/denops.vim" } },
+  { "lewis6991/gitsigns.nvim" },
 
-  use({ "vim-test/vim-test" })
+  { "vim-test/vim-test" },
 
-  use({ "nvim-tree/nvim-tree.lua", requires = { "nvim-tree/nvim-web-devicons" } })
-  use({ "github/copilot.vim" })
+  { "nvim-tree/nvim-tree.lua", dependencies = { "nvim-tree/nvim-web-devicons" } },
+  { "github/copilot.vim" },
 
-  use({ "skanehira/denops-docker.vim", requires = "vim-denops/denops.vim" })
-  use({ "vim-skk/skkeleton", requires = "vim-denops/denops.vim" })
-  use({ "uga-rosa/cmp-skkeleton" })
+  { "skanehira/denops-docker.vim", dependencies = "vim-denops/denops.vim" },
+  { "vim-skk/skkeleton", dependencies = "vim-denops/denops.vim" },
+  { "uga-rosa/cmp-skkeleton" },
 
-  use({ "tyru/open-browser-github.vim", requires = "tyru/open-browser.vim" })
+  { "tyru/open-browser-github.vim", dependencies = "tyru/open-browser.vim" },
 
-  use({ "thinca/vim-quickrun", requires = "lambdalisue/vim-quickrun-neovim-job" })
-  use({ "folke/which-key.nvim" })
+  { "thinca/vim-quickrun", dependencies = "lambdalisue/vim-quickrun-neovim-job" },
+  { "folke/which-key.nvim" },
+})
 
-  if packer_bootstrap then
-    require("packer").sync()
-  end
-end)

1e9c3519f78152f107d68f51d31442708e8c4589
Yoichi Takai <contact@peacock0803sz.com>
2023-05-16 05:26:40 +0900

    update nvim

---
+++ .config/nvim/lua/plugins/ccc.lua
@@ -0,0 +1,11 @@
+local function config()
+  require("ccc").setup({
+    highlighter = {
+      auto_enable = true,
+      lsp = true,
+    },
+  })
+end
+
+local spec = {"uga-rosa/ccc.nvim", config = config}
+return spec

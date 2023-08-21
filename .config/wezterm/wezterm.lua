63b204d8f9d2c3227523d8e4c7ecbc7f1e621647
Peacock <contact@peacock0803sz.com>
2023-08-21 09:21:11 +0900

    Update wezterm
    thx: https://github.com/NI57721/dotfiles/commit/40e936e05836ec475aece133d458084f2fc09c17

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,20 +1,77 @@
 local wezterm = require("wezterm")
 
-local function get_appearance()
-  if wezterm.gui then
-    return wezterm.gui.get_appearance()
-  else -- default to dark
-    return "Dark"
+local function basename(s)
+  return string.gsub(s, '(.*[/\\])(.*)', '%2')
+end
+
+---@param s string
+---@return string
+local function omit_path(s)
+  local r = s:gsub('^[^:]*://[^/]*', ''):gsub('^' .. wezterm.home_dir, '~'):gsub('([^/])[^/]*/', '%1/')
+  return r
+end
+
+---@param tab_info any
+---@return string
+local function make_title(tab_info)
+  ---@type string
+  local process_name = basename(tab_info.active_pane.foreground_process_name)
+
+  if process_name == 'zsh' then
+    return omit_path(tab_info.active_pane.current_working_dir)
+  elseif process_name ~= '' then
+    return process_name
+  else
+    return ""
   end
 end
 
--- local function get_colorscheme()
---   if get_appearance() == "Light" then
---     return "dawnfox"
---   else
---     return "nordfox"
---   end
--- end
+---@param tab any
+---@param tabs any
+---@param panes any
+---@param config any
+---@param hover any
+---@param max_width any
+---@return table
+local function format_tab_title(tab, tabs, panes, config, hover, max_width)
+  local white = "#cdcecf"
+  local black = "#2e3440"
+  local title = tab.tab_index + 1 .. ': ' .. make_title(tab)
+
+  if tab.is_active then
+    return {
+      { Background = { Color = white } },
+      { Foreground = { Color = black } },
+      { Text = title },
+    }
+  else
+    return {
+      { Background = { Color = black } },
+      { Foreground = { Color = white } },
+      { Text = title },
+    }
+  end
+end
+
+wezterm.on(
+  'format-tab-title', format_tab_title
+)
+
+---comment
+---@param tab any
+---@param pane any
+---@param tabs any
+---@param panes any
+---@param config any
+---@return string
+local function format_window_title(tab, pane, tabs, panes, config)
+  local workspace = wezterm.mux.get_active_workspace()
+  return "[" .. workspace .. "]: " .. make_title(tab)
+end
+
+wezterm.on(
+  'format-window-title', format_window_title
+)
 
 wezterm.on("window-config-reloaded", function(window, _)
   window:toast_notification("wezterm", "configuration reloaded!", nil, 4000)
@@ -30,7 +87,7 @@ return {
 
   -- Fonts
   font = wezterm.font_with_fallback({
-    { family = "UDEV Gothic", weight = "Bold" },
+    { family = "UDEV Gothic",    weight = "Bold" },
     { family = "UDEV Gothic NF", weight = "Bold" },
     "Noto Color Emoji",
   }),
@@ -57,16 +114,16 @@ return {
       mods = "LEADER",
       action = wezterm.action({ Search = { CaseSensitiveString = "" } }),
     },
-    { key = "q", mods = "CMD", action = "QuitApplication" },
+    { key = "q", mods = "CMD",       action = "QuitApplication" },
 
     -- Clipboards
-    { key = "c", mods = "SUPER", action = wezterm.action({ CopyTo = "Clipboard" }) },
-    { key = "v", mods = "SUPER", action = wezterm.action({ PasteFrom = "Clipboard" }) },
+    { key = "c", mods = "SUPER",     action = wezterm.action({ CopyTo = "Clipboard" }) },
+    { key = "v", mods = "SUPER",     action = wezterm.action({ PasteFrom = "Clipboard" }) },
 
     -- Font & Window Sizing
     { key = "+", mods = "ALT|SHIFT", action = "IncreaseFontSize" },
-    { key = "-", mods = "ALT", action = "DecreaseFontSize" },
-    { key = "0", mods = "ALT", action = "ResetFontSize" },
+    { key = "-", mods = "ALT",       action = "DecreaseFontSize" },
+    { key = "0", mods = "ALT",       action = "ResetFontSize" },
     { key = "0", mods = "ALT|SHIFT", action = "ResetFontAndWindowSize" },
 
     -- Workspaces
@@ -76,8 +133,8 @@ return {
       action = wezterm.action.ShowLauncherArgs({ flags = "FUZZY|WORKSPACES" }),
     },
     { key = "s", mods = "ALT|SHIFT", action = wezterm.action({ SwitchToWorkspace = {} }) },
-    { key = "l", mods = "ALT", action = wezterm.action({ SwitchWorkspaceRelative = 1 }) },
-    { key = "h", mods = "ALT", action = wezterm.action({ SwitchWorkspaceRelative = -1 }) },
+    { key = "l", mods = "ALT",       action = wezterm.action({ SwitchWorkspaceRelative = 1 }) },
+    { key = "h", mods = "ALT",       action = wezterm.action({ SwitchWorkspaceRelative = -1 }) },
     -- {key="1", mods="ALT|SHIFT", action=wezterm.action{SwitchToWorkspace={name="0"}}},
     -- {key="2", mods="ALT|SHIFT", action=wezterm.action{SwitchToWorkspace={name="1"}}},
     -- {key="3", mods="ALT|SHIFT", action=wezterm.action{SwitchToWorkspace={name="2"}}},
@@ -127,9 +184,9 @@ return {
     { key = "j", mods = "ALT|SHIFT", action = wezterm.action({ ActivatePaneDirection = "Down" }) },
     { key = "k", mods = "ALT|SHIFT", action = wezterm.action({ ActivatePaneDirection = "Up" }) },
     { key = "l", mods = "ALT|SHIFT", action = wezterm.action({ ActivatePaneDirection = "Right" }) },
-    { key = "h", mods = "LEADER", action = wezterm.action({ AdjustPaneSize = { "Left", 5 } }) },
-    { key = "j", mods = "LEADER", action = wezterm.action({ AdjustPaneSize = { "Down", 5 } }) },
-    { key = "k", mods = "LEADER", action = wezterm.action({ AdjustPaneSize = { "Up", 5 } }) },
-    { key = "l", mods = "LEADER", action = wezterm.action({ AdjustPaneSize = { "Right", 5 } }) },
+    { key = "h", mods = "LEADER",    action = wezterm.action({ AdjustPaneSize = { "Left", 5 } }) },
+    { key = "j", mods = "LEADER",    action = wezterm.action({ AdjustPaneSize = { "Down", 5 } }) },
+    { key = "k", mods = "LEADER",    action = wezterm.action({ AdjustPaneSize = { "Up", 5 } }) },
+    { key = "l", mods = "LEADER",    action = wezterm.action({ AdjustPaneSize = { "Right", 5 } }) },
   },
 }

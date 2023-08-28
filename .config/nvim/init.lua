45891773887c967f2a01352bd0d92cde4b38c9c2
Peacock <contact@peacock0803sz.com>
2023-08-28 10:51:03 +0900

    Update

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,5 +1,10 @@
 require("options")
 
+if vim.env.DENOPS_DEBUG then
+  vim.g["denops#debug"] = true
+  vim.g["denops#trace"] = true
+end
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({

cfd1f2f86601f9aca8c87303600afbca3be99f9e
denopendabot <denopendabot@github.com>
2023-09-02 00:31:34 +0000

    build(deps): bump std from 0.200.0 to 0.201.0

--- .config/nvim/ddu/main.ts
+++ .config/nvim/ddu/main.ts
@@ -1,4 +1,4 @@
-import * as stdpath from "https://deno.land/std@0.200.0/path/mod.ts";
+import * as stdpath from "https://deno.land/std@0.201.0/path/mod.ts";
 import {
   ActionArguments,
   ActionFlags,

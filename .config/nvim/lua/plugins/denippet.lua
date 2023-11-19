6599575f311f12fbccec8d30d103603df5341779
Peacock <me@p3ac0ck.net>
2023-11-19 13:37:40 +0900

    LuaSnip to denippet

---
+++ .config/nvim/lua/plugins/denippet.lua
@@ -0,0 +1,14 @@
+local function config()
+  local dir = vim.fn.stdpath("config") .. "/snippets/"
+  for name, _ in vim.fs.dir(dir) do
+    vim.fn["denippet#load"](dir .. name)
+  end
+end
+
+---@type LazySpec
+local spec = {
+  "https://github.com/uga-rosa/denippet.vim",
+  dependencies = { "denops.vim", "ddc.vim" },
+  config = config
+}
+return spec

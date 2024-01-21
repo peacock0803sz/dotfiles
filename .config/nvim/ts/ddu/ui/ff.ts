9aadc4b79e63d81c9949a21db89c56ba49592020
denopendabot <denopendabot@github.com>
2024-01-21 00:05:34 +0000

    build(deps): bump ddu_vim from v3.9.0 to v3.10.0

--- .config/nvim/ts/ddu/ui/ff.ts
+++ .config/nvim/ts/ddu/ui/ff.ts
@@ -1,5 +1,5 @@
-import { BaseConfig } from "https://deno.land/x/ddu_vim@v3.9.0/types.ts";
-import { ConfigArguments } from "https://deno.land/x/ddu_vim@v3.9.0/base/config.ts";
+import { BaseConfig } from "https://deno.land/x/ddu_vim@v3.10.0/types.ts";
+import { ConfigArguments } from "https://deno.land/x/ddu_vim@v3.10.0/base/config.ts";
 import { Params as FfParams } from "https://deno.land/x/ddu_ui_ff@v1.1.0/ff.ts";
 
 import getSettings from "../settings.ts";

8b5e472eecc6bbb0b8ceedb5e280f82729350f29
denopendabot <denopendabot@github.com>
2024-02-07 00:04:30 +0000

    build(deps): bump unknownutil from v3.14.1 to v3.15.0

--- .config/nvim/ts/ddu/main.ts
+++ .config/nvim/ts/ddu/main.ts
@@ -7,7 +7,7 @@ import {
 import { fn } from "https://deno.land/x/ddu_vim@v3.10.2/deps.ts";
 import { ConfigArguments } from "https://deno.land/x/ddu_vim@v3.10.2/base/config.ts";
 import { ActionData } from "https://deno.land/x/ddu_kind_file@v0.7.1/file.ts";
-import * as u from "https://deno.land/x/unknownutil@v3.14.1/mod.ts";
+import * as u from "https://deno.land/x/unknownutil@v3.15.0/mod.ts";
 
 type Never = Record<never, never>;
 

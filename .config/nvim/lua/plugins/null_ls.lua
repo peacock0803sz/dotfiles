372d57d13637112435324268a33cda7522a6fc5f
Peacock <me@p3ac0ck.net>
2024-02-25 20:12:30 +0900

    disable shellcheck

--- .config/nvim/lua/plugins/null_ls.lua
+++ .config/nvim/lua/plugins/null_ls.lua
@@ -7,12 +7,6 @@ local function config()
     --   extra_args = { "--max-line-length", "88" },
     -- }),
     -- null_ls.builtins.diagnostics.ruff,
-    null_ls.builtins.diagnostics.shellcheck.with({
-      filetypes = { "sh", "bash", "zsh" },
-    }),
-    null_ls.builtins.code_actions.shellcheck.with({
-      filetypes = { "sh", "bash", "zsh" },
-    }),
     -- JavaScript
     null_ls.builtins.formatting.prettier,
     -- Python

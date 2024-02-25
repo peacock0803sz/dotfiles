213540c770d813ea91927f8c5f12e08ef644e754
Peacock <me@p3ac0ck.net>
2024-02-25 23:24:42 +0900

    hijack netrw to ddu filer

--- .config/nvim/lua/plugins/ddu/ui/filer.lua
+++ .config/nvim/lua/plugins/ddu/ui/filer.lua
@@ -1,12 +1,35 @@
+local function suppress_netrw()
+  if vim.fn.exists("#FileExplorer") then
+    vim.api.nvim_clear_autocmds({ group = "FileExplorer" })
+  end
+end
+
+local function hijack_netrw()
+  vim.api.nvim_create_user_command("DduFiler", function()
+    local p = vim.fn.expand("%:p")
+    vim.fn["ddu#custom#patch_global"]("sourceOptions", { file = { path = vim.fn.expand(p) } })
+    vim.fn["ddu#start"]({ name = "file", ui = "filer", sources = { { name = "file" } } })
+  end, {})
+
+  local path = vim.fn.expand("%:p")
+  if vim.fn.isdirectory(path) == 0 then
+    return nil
+  end
+
+  local bufnr = vim.fn.bufnr()
+  vim.fn.execute("keepjumps keepalt DduFiler")
+  vim.fn.execute(vim.fn.printf("silent! bwipeout %d", bufnr))
+end
+
 local function config()
   local config_dir = vim.fn.stdpath("config")
   local path = config_dir .. "/ts/ddu/ui/filer.ts"
   vim.fn["ddu#custom#load_config"](path)
 
-  local group = vim.api.nvim_create_augroup("plug-ddu-ui-filer", { clear = true })
+  local group_map = vim.api.nvim_create_augroup("plug-ddu-ui-filer", { clear = true })
   vim.api.nvim_create_autocmd("FileType", {
     pattern = "ddu-filer",
-    group = group,
+    group = group_map,
     callback = function()
       local helper = require("plugins.ddu.map").map_action
 
@@ -29,6 +52,19 @@ local function config()
       helper("n", "l", "expandItem")
       helper("n", "h", "collapseItem")
     end,
+  })
+
+  local group_hijack = vim.api.nvim_create_augroup("hijack-netrw", { clear = true })
+  vim.api.nvim_create_autocmd("VimEnter", {
+    pattern = "*",
+    group = group_hijack,
+    callback = suppress_netrw,
+  })
+  vim.api.nvim_create_autocmd("BufEnter", {
+    pattern = "*",
+    nested = true,
+    group = group_hijack,
+    callback = hijack_netrw,
   })
 end
 

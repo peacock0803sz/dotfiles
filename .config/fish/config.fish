aefdec1da3c7877bffb0a23637d772bf674647c4
Peacock <me@p3ac0ck.net>
2024-07-22 17:42:44 +0900

    direnv fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -101,3 +101,7 @@ abbr --add tf terraform
 if test -e "/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.fish"
     . "/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.fish"
 end
+
+# direnv {{{
+direnv hook fish | source
+#   }}}

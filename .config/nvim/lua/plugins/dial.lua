db4f807584cf7cee45baee40b803b126dcd5469c
Peacock <me@p3ac0ck.net>
2024-08-07 20:33:21 +0900

    Update

--- .config/nvim/lua/plugins/dial.lua
+++ .config/nvim/lua/plugins/dial.lua
@@ -4,7 +4,8 @@ local function config()
     default = {
       augend.integer.alias.decimal, -- nonnegative decimal number (0, 1, 2, 3, ...)
       augend.integer.alias.hex, -- nonnegative hex number  (0x01, 0x1a1f, etc.)
-      augend.date.alias["%Y/%m/%d"], -- date (2022/02/19, etc.)
+      augend.date.new({ pattern = "%Y/%m/%d", default_kind = "day" }),
+      augend.date.new({ pattern = "%Y-%m-%d", default_kind = "day" }),
     },
   })
 
--- flake.nix
+++ flake.nix
@@ -6,9 +6,13 @@
     flake-utils.inputs.system.follows = "systems";
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
+    vim-src = {
+      url = "github:vim/vim";
+      flake = false;
+    };
   };
 
-  outputs = { nixpkgs, neovim-nightly-overlay, flake-utils, ... }:
+  outputs = { nixpkgs, vim-src, neovim-nightly-overlay, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
@@ -57,6 +61,7 @@
 
           # LSP/Formatter
           lua-language-server
+          bash-language-server
           dockerfile-language-server-nodejs
           nixd
           pyright
@@ -71,12 +76,26 @@
           vue-language-server
           yaml-language-server
         ];
+        vim = pkgs.vim.overrideAttrs (old: {
+          version = "latest";
+          src = vim-src;
+          buildInputs = old.buildInputs ++ ( with pkgs; [gettext lua libiconv] );
+          configureFlags = old.configureFlags ++
+            [
+              "--enable-terminal"
+              "--with-compiledby=Peaocck (Yoichi Takai)"
+              "--enable-luainterp"
+              "--with-lua-prefix=${pkgs.lua}"
+              "--enable-fail-if-missing"
+            ];
+        });
+        neovim = neovim-nightly-overlay.packages.${system}.default ;
       in
       {
         formatter = pkgs.nixpkgs-fmt;
         packages.default = pkgs.buildEnv {
-          name = "default-packages";
-          paths = [ neovim-nightly-overlay.packages.${system}.default ] ++ packages;
+          name = "default";
+          paths = packages ++ [ vim neovim ];
         };
       });
 }

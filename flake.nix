e15d5cf59916b6b44d031ecc79a3f6b8fa081b52
Peacock <me@p3ac0ck.net>
2024-08-31 19:00:13 +0900

    Update nix flake

--- flake.nix
+++ flake.nix
@@ -6,13 +6,14 @@
     flake-utils.inputs.system.follows = "systems";
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
-    vim-src = {
-      url = "github:vim/vim";
-      flake = false;
-    };
+    # vim-src = {
+    #   url = "github:vim/vim";
+    #   flake = false;
+    # };
   };
 
-  outputs = { nixpkgs, vim-src, neovim-nightly-overlay, flake-utils, ... }:
+  # outputs = { nixpkgs, vim-src, neovim-nightly-overlay, flake-utils, ... }:
+  outputs = { nixpkgs, neovim-nightly-overlay, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
@@ -21,14 +22,21 @@
           overlays = [ neovim-nightly-overlay.overlays.default ];
         };
         packages = with pkgs; [
+          # GUI Apps
+          # wezterm
+          # alacritty
+          # raycast
+
           _1password
           direnv
           nix-direnv
           eza
+          # delta
           fd
           ffmpeg
           fish
           fzf
+          gibo
           gojq
           gh
           ghq
@@ -38,6 +46,8 @@
           imagemagick
           nodePackages.prettier
           ripgrep
+          tmux
+          wget
 
           # Language Runtime
           bun
@@ -46,6 +56,7 @@
           go
           nodejs_20
           python3
+          uv
           rustc
           terraform
           typescript
@@ -56,13 +67,15 @@
           sqlite
 
           # Public Cloud
-          awscli2
+          act
+          # awscli2
           google-cloud-sdk
 
           # LSP/Formatter
           lua-language-server
           bash-language-server
           dockerfile-language-server-nodejs
+          gopls
           nixd
           pyright
           ruff-lsp
@@ -76,26 +89,27 @@
           vue-language-server
           yaml-language-server
         ];
-        vim = pkgs.vim.overrideAttrs (old: {
-          version = "latest";
-          src = vim-src;
-          buildInputs = old.buildInputs ++ ( with pkgs; [gettext lua libiconv] );
-          configureFlags = old.configureFlags ++
-            [
-              "--enable-terminal"
-              "--with-compiledby=Peaocck (Yoichi Takai)"
-              "--enable-luainterp"
-              "--with-lua-prefix=${pkgs.lua}"
-              "--enable-fail-if-missing"
-            ];
-        });
-        neovim = neovim-nightly-overlay.packages.${system}.default ;
+        # vim = pkgs.vim.overrideAttrs (old: {
+        #   version = "latest";
+        #   src = vim-src;
+        #   buildInputs = old.buildInputs ++ (with pkgs; [ gettext lua libiconv ]);
+        #   configureFlags = old.configureFlags ++
+        #     [
+        #       "--enable-terminal"
+        #       "--with-compiledby=Peaocck (Yoichi Takai)"
+        #       "--enable-luainterp"
+        #       "--with-lua-prefix=${pkgs.lua}"
+        #       "--enable-fail-if-missing"
+        #     ];
+        # });
+        neovim = neovim-nightly-overlay.packages.${system}.default;
       in
       {
         formatter = pkgs.nixpkgs-fmt;
         packages.default = pkgs.buildEnv {
           name = "default";
-          paths = packages ++ [ vim neovim ];
+          # paths = packages ++ [ vim neovim  ];
+          paths = packages ++ [ neovim ];
         };
       });
 }

d9ed8e24870c5244cc4da8920aa04a4c891a58e0
Peacock <me@p3ac0ck.net>
2025-02-17 12:34:02 +0900

    Update ddu.vim

--- .config/nvim/ts/ddu/ui/ff.ts
+++ .config/nvim/ts/ddu/ui/ff.ts
@@ -6,7 +6,12 @@ export class Config extends BaseConfig {
   override async config(args: ConfigArguments): Promise<void> {
     args.contextBuilder.patchGlobal({
       ui: "ff",
-      uiParams: { ff: (await getSettings(args.denops)) as FfParams },
+      uiParams: {
+        ff: {
+          displayTree: true,
+          ...(await getSettings(args.denops)),
+        } as FfParams,
+      },
     });
     return Promise.resolve();
   }

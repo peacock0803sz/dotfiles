ab895329f6c86140e024898598b4afb0333f54b5
Peacock <me@p3ac0ck.net>
2025-10-07 09:53:11 +0900

    Update ghosttext nvim

--- .config/nvim/lua/plugins/ghost.lua
+++ .config/nvim/lua/plugins/ghost.lua
@@ -1,15 +1,9 @@
 local function config()
-  if vim.env.NVIM_GHOST == nil then
-    vim.g["nvim_ghost_autostart"] = 0
-  else
-    vim.g["nvim_ghost_autostart"] = 1
-  end
   vim.g["nvim_ghost_server_port"] = 14001
 
   local group = vim.api.nvim_create_augroup("nvim_ghost_user_autocommands", { clear = false })
   local filetypes = {
-    markdown = { "*github.com", "*backlog.com" },
-    html = { "*slack.com", "*zendesk.com" },
+    markdown = { "*github.com" }
   }
   for ft, pattern in pairs(filetypes) do
     vim.api.nvim_create_autocmd(

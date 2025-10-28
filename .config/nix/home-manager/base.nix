044606349072f7a334f06af0e1408f6a6f9636e9
Peacock <me@p3ac0ck.net>
2025-10-28 15:36:05 +0900

    Use production zeno.zsh

--- .config/nix/home-manager/base.nix
+++ .config/nix/home-manager/base.nix
@@ -15,14 +15,12 @@ in
     }).outPath + "/gh-copilot-alias.fish";
 
     # zeno.zsh {{{
-    ".local/share/fish/plugins/zeno.zsh".source = mkOutOfStoreSymlink
-      "${config.home.homeDirectory}/ghq/github.com/peacock0803sz/zeno.zsh";
-    # ".local/share/fish/plugins/zeno.zsh".source = (pkgs.fetchgit {
-    #   url = "https://github.com/yuki-yano/zeno.zsh";
-    #   hash = "sha256-fk4CEGd2CufYwN3kgoJ0tpfuz7GHQdU+2SLYRBmZ10Q=";
-    # }).outPath;
-    ".config/fish/conf.d/zeno.fish".source = mkOutOfStoreSymlink
-      "${config.home.homeDirectory}/.local/share/fish/plugins/zeno.zsh/shells/fish/conf.d/zeno.fish";
+    ".local/share/fish/plugins/zeno.zsh".source = (pkgs.fetchgit {
+      url = "https://github.com/yuki-yano/zeno.zsh";
+      hash = "sha256-fk4CEGd2CufYwN3kgoJ0tpfuz7GHQdU+2SLYRBmZ10Q=";
+    }).outPath;
+    # ".config/fish/conf.d/zeno.fish".source = mkOutOfStoreSymlink
+    #   "${config.home.homeDirectory}/.local/share/fish/plugins/zeno.zsh/shells/fish/conf.d/zeno.fish";
     ".config/zeno".source = mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/.config/zeno";
     # }}}
   };

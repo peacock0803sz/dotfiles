0dea0fef385bd2109afb3fc256a937ed07e9aff6
Peacock <33555487+peacock0803sz@users.noreply.github.com>
2025-12-28 13:30:17 +0900

    Update ssh config

--- .config/nix/home-manager/darwin.nix
+++ .config/nix/home-manager/darwin.nix
@@ -6,6 +6,8 @@ in
   home.file = {
     ".gitconfig".source = mkOutOfStoreSymlink
       "${config.home.homeDirectory}/dotfiles/.config/git/.gitconfig.darwin";
+    ".ssh/config".source = mkOutOfStoreSymlink
+      "${config.home.homeDirectory}/dotfiles/.config/ssh/config.darwin";
     ".config/karabiner/karabiner.json".source = mkOutOfStoreSymlink
       "${config.home.homeDirectory}/dotfiles/.config/karabiner/karabiner.json";
     ".config/nvim.aibo".source = mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/.config/nvim.aibo";

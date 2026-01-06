a7f600a0626fa832e93050fe4d663774f5d5e93a
Peacock <33555487+peacock0803sz@users.noreply.github.com>
2026-01-06 20:01:06 +0900

    Update tide variables

--- .config/fish/tide.fish
+++ .config/fish/tide.fish
@@ -1,27 +1,27 @@
-set --universal tide_character_icon '$'
-set --universal tide_character_color green
+set tide_character_icon '$'
+set tide_character_color green
 
-set --universal tide_status_color green
-set --universal tide_status_color_failure red
+set tide_status_color green
+set tide_status_color_failure red
 
-set --universal tide_pwd_color_anchors blue
-set --universal tide_pwd_color_dirs blue
-set --universal tide_pwd_color_truncated_dirs blue
+set tide_pwd_color_anchors blue
+set tide_pwd_color_dirs blue
+set tide_pwd_color_truncated_dirs blue
 
-set --universal tide_git_color_branch green
-set --universal tide_git_color_conflicted red
-set --universal tide_git_color_operation red
-set --universal tide_git_color_stash green
-set --universal tide_git_color_dirty yellow
-set --universal tide_git_color_staged yellow
-set --universal tide_git_color_untracked blue
-set --universal tide_git_color_modified yellow
+set tide_git_color_branch green
+set tide_git_color_conflicted red
+set tide_git_color_operation red
+set tide_git_color_stash green
+set tide_git_color_dirty yellow
+set tide_git_color_staged yellow
+set tide_git_color_untracked blue
+set tide_git_color_modified yellow
 
-set --universal tide_right_prompt_items status context jobs direnv node python rustc java php pulumi ruby go gcloud terraform aws nix_shell crystal elixir zig
-set --universal tide_kubectl_color blue
-set --universal tide_docker_color blue
-set --universal tide_node_color green
-set --universal tide_ruby_color red
-set --universal tide_rust_color yellow
-set --universal tide_python_color blue
-set --universal tide_terraform_color blue
+set tide_right_prompt_items status context jobs direnv node python rustc java php pulumi ruby go gcloud terraform aws nix_shell crystal elixir zig
+set tide_kubectl_color blue
+set tide_docker_color blue
+set tide_node_color green
+set tide_ruby_color red
+set tide_rust_color yellow
+set tide_python_color blue
+set tide_terraform_color blue

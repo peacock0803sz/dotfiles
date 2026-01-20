0cd048932d638ba91954fb03f7f77cc98e61c894
Peacock <33555487+peacock0803sz@users.noreply.github.com>
2026-01-20 11:16:45 +0900

    Update nocturne

--- .config/nix/hosts/nocturne/default.nix
+++ .config/nix/hosts/nocturne/default.nix
@@ -16,18 +16,6 @@ let
 
   npmPkgs = pkgs.callPackage ./node2nix { inherit pkgs; };
   brewCasks = import ./brewCasks.nix;
-  homeModules = [
-    (import ./home.nix)
-    (import ../../home-manager )
-    (import ../../home-manager/platforms/darwin.nix )
-    (import ../../home-manager/programs/alacritty.nix )
-    (import ../../home-manager/programs/claude-code { inherit pkgs npmPkgs mcp-servers-nix; })
-    (import ../../home-manager/programs/direnv.nix )
-    (import ../../home-manager/programs/ghostty.nix )
-    (import ../../home-manager/programs/neovim.nix )
-    (import ../../home-manager/programs/tmux.nix )
-    (import ../../home-manager/programs/wezterm.nix )
-  ];
 in
 nix-darwin.lib.darwinSystem {
   modules = [
@@ -36,8 +24,20 @@ nix-darwin.lib.darwinSystem {
     (import ../../nix-darwin/lemonade.nix { inherit pkgs; })
     {
       home-manager.backupFileExtension = "bk.nix";
+      home-manager.extraSpecialArgs = { inherit npmPkgs mcp-servers-nix; };
       home-manager.users.${username} = {
-        imports = homeModules;
+        imports = [
+          ./home.nix
+          ../../home-manager
+          ../../home-manager/platforms/darwin.nix
+          ../../home-manager/programs/alacritty.nix
+          ../../home-manager/programs/claude-code
+          ../../home-manager/programs/direnv.nix
+          ../../home-manager/programs/ghostty.nix
+          ../../home-manager/programs/neovim.nix
+          ../../home-manager/programs/tmux.nix
+          ../../home-manager/programs/wezterm.nix
+        ];
 
         home.packages = import ./packages.nix { inherit pkgs npmPkgs; };
       };

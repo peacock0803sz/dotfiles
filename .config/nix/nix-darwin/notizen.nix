2fbd7c09cbd0690ad86d0f5bbf73f2ce7d80a295
Peacock <33555487+peacock0803sz@users.noreply.github.com>
2026-01-22 23:12:00 +0900

    Update notizen in nix-darwin

--- .config/nix/nix-darwin/notizen.nix
+++ .config/nix/nix-darwin/notizen.nix
@@ -7,12 +7,12 @@
           NOTIZEN_REMOTE_HOST = "enigma.tail2121a.ts.net";
           NOTIZEN_REMOTE_USER = "${inputs.username}";
           NOTIZEN_REMOTE_KEY = "/Users/${inputs.username}/.ssh/id_ed25519";
-          NOTIZEN_REMOTE_PATH = "/home/${inputs.username}/notizen/";
+          NOTIZEN_REMOTE_PATH = "/home/${inputs.username}/notizen/source/";
         };
         ProgramArguments = [
           "/Users/${inputs.username}/notizen/.venv/bin/notizen"
           "sync"
-          "/Users/${inputs.username}/notizen/source"
+          "--src=/Users/${inputs.username}/notizen/source"
         ];
         StartInterval = 3600;
         WorkingDirectory = "/Users/${inputs.username}/notizen";

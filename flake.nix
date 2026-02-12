beee1c60ddde8d9cea3061f869be71ef3e360bfb
Peacock <33555487+peacock0803sz@users.noreply.github.com>
2026-02-12 22:06:20 +0900

    Update flake inputs

--- flake.nix
+++ flake.nix
@@ -2,8 +2,7 @@
   description = "Peacock's Nix Configuration";
 
   inputs = {
-    # nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    nixpkgs.url = "github:NixOS/nixpkgs?rev=2d293cbfa5a793b4c50d17c05ef9e385b90edf6c";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-index-database = {
       url = "github:nix-community/nix-index-database";
       inputs.nixpkgs.follows = "nixpkgs";

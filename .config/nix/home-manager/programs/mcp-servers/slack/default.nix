bad85d6144d7dc2d57ae732dd44395cc93e8be56
Peacock <33555487+peacock0803sz@users.noreply.github.com>
2026-02-28 20:32:29 +0900

    Update

--- .config/nix/home-manager/programs/mcp-servers/slack/default.nix
+++ .config/nix/home-manager/programs/mcp-servers/slack/default.nix
@@ -1,5 +1,5 @@
 { pkgs }: {
   command = "${pkgs.lib.getExe' pkgs.nodejs "npx"}";
   args = [ "@modelcontextprotocol/server-slack" ];
-  passwordCommand = "echo SLACK_BOT_TOKEN=$SLACK_USER_TOKEN SLACK_TEAM_ID=$SLACK_TEAM_ID";
+  passwordCommand = "echo SLACK_BOT_TOKEN=$SLACK_BOT_TOKEN SLACK_TEAM_ID=$SLACK_TEAM_ID";
 }

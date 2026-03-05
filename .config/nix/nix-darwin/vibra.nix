d5a789609452301c10cdee4ca66da172dc15d315
Peacock <33555487+peacock0803sz@users.noreply.github.com>
2026-03-05 20:48:01 +0900

    Update vibra

--- .config/nix/nix-darwin/vibra.nix
+++ .config/nix/nix-darwin/vibra.nix
@@ -5,7 +5,6 @@
     enable = true;
     backend = {
       allowedDirs = [ "/Users/${args.username}/src" ];
-      devUser = args.username;
       defaultWorkdir = "/Users/${args.username}/src";
     };
     frontend.enable = false;
--- .config/nix/nixos/vibra.nix
+++ .config/nix/nixos/vibra.nix
@@ -4,8 +4,9 @@
   services.vibra = {
     enable = true;
     backend = {
+      listenAddr = "0.0.0.0:3001";
+      corsOrigin = "http://enigma.tail2121a.ts.net:3000";
       allowedDirs = [ "/home/${args.username}/src" ];
-      devUser = args.username;
       defaultWorkdir = "/home/${args.username}/src";
       environmentFile = "/var/lib/vibra/env";
     };

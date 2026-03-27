9191d18b8e04ad6f2ab207735fddecb141220982
Peacock <33555487+peacock0803sz@users.noreply.github.com>
2026-03-27 17:29:23 +0900

    Remove working MCPs alreadly configured org-global

--- .config/nix/home-manager/programs/mcp-servers/slack/default.nix
+++
@@ -1,5 +0,0 @@
-{ pkgs }: {
-  command = "${pkgs.lib.getExe' pkgs.nodejs "npx"}";
-  args = [ "@modelcontextprotocol/server-slack" ];
-  passwordCommand = "echo SLACK_BOT_TOKEN=$SLACK_BOT_TOKEN SLACK_TEAM_ID=$SLACK_TEAM_ID";
-}

2848be3f29e82de94694a01d14ab609b199d766d
Peacock <33555487+peacock0803sz@users.noreply.github.com>
2026-03-28 00:01:56 +0900

    Add template registry

--- .config/nix/home-manager/default.nix
+++ .config/nix/home-manager/default.nix
@@ -1,3 +1,9 @@
 { ... }: {
   home.stateVersion = "25.11";
+  nix.registry = {
+    templates = {
+      from = { type = "indirect"; id = "templa"; };
+      to = { type = "github"; owner = "peacock0803sz"; repo = "templa"; };
+    };
+  };
 }

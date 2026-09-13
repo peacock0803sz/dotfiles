db9cd919581dfec8352db46698bd05ee7d91da7b
Peacock <33555487+peacock0803sz@users.noreply.github.com>
2026-09-13 22:33:17 +0900

    Update codex

--- dot_config/nix/home-manager/programs/codex.nix
+++ dot_config/nix/home-manager/programs/codex.nix
@@ -1,4 +1,4 @@
-{ pkgs, inputs, llm-agents, ... }:
+{ pkgs, inputs, llm-agents, hostName, ... }:
 let
   inherit (inputs) mcp-servers-nix;
   config = mcp-servers-nix.lib.mkConfig pkgs {
@@ -6,7 +6,26 @@ let
     format = "toml-inline";
     fileName = ".mcp.toml";
     programs = import ./mcp-servers/programs.nix { inherit pkgs mcp-servers-nix; };
-    settings.servers.linear = import ./mcp-servers/linear { inherit pkgs; };
+    settings.servers = {
+      kubernetes = import ./mcp-servers/kubernetes { inherit pkgs; };
+      git_p3ac0ck_net = (import ./mcp-servers/gitea {
+        inherit pkgs;
+        host = "https://git.p3ac0ck.net";
+        tokenSuffix = "git_p3ac0ck_net";
+      });
+    } // (if hostName == "arpeggio" then {
+      devin = (import ./mcp-servers/devin { inherit pkgs; });
+      # esa = (import ./mcp-servers/esa { inherit pkgs; });
+      gcloud = (import ./mcp-servers/gcloud { inherit pkgs; });
+      gitea_groove-x_io = (import ./mcp-servers/gitea {
+        inherit pkgs;
+        host = "https://gitea.groove-x.io";
+        tokenSuffix = "gitea_groove_x_io";
+      });
+      lovot = (import ./mcp-servers/lovot { inherit pkgs; });
+      # wrike = (import ./mcp-servers/wrike { inherit pkgs; });
+      # slack = (import ./mcp-servers/slack { inherit pkgs; });
+    } else { });
   };
 in
 {

ed3f6bb827cbec920c979b274cbe1e87b23384fc
Peacock <33555487+peacock0803sz@users.noreply.github.com>
2026-09-21 11:45:43 +0900

    Update lemonade

--- dot_config/nix/nix-darwin/lemonade.nix
+++ dot_config/nix/nix-darwin/lemonade.nix
@@ -1,4 +1,12 @@
-{ pkgs, ... }: {
+{ lib, pkgs, ... }:
+let
+  lemonadeArgs = [
+    "${pkgs.lemonade}/bin/lemonade"
+    "server"
+    "--allow=0.0.0.0/0"
+  ];
+in
+{
   launchd.user.agents.lemonade = {
     serviceConfig = {
       EnvironmentVariables = {
@@ -6,9 +14,9 @@
         LC_ALL = "ja_JP.UTF-8";
       };
       ProgramArguments = [
-        "${pkgs.lemonade}/bin/lemonade"
-        "server"
-        "--allow=0.0.0.0/0"
+        "/bin/sh"
+        "-c"
+        "/bin/wait4path /nix/store && exec ${lib.escapeShellArgs lemonadeArgs}"
       ];
       KeepAlive = true;
       StandardErrorPath = "/tmp/lemonade.err.log";
